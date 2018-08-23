76784792bff20daa063d63a5447e71145935e260
H2V <h2v9696@gmail.com>
2018-08-23 12:43:29 +0700

    Remove unnessesary file

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
   , router = express.Router()
 
-router.use('/comments', require('./comments'))
 router.use('/users', require('./users'))
 
 router.get('/', function(req, res) {
