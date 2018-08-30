e41c37f71368700e1ef5a078082a0dbc8a9f777e
ChinhLQR <chinhhedspibk@gmail.com>
2018-08-30 15:42:12 +0700

    function resize + tap

--- public/code/game.js
+++ public/code/game.js
@@ -2,6 +2,7 @@
 //function when they've loaded
 let app = new PIXI.Application(400, 600, {backgroundColor : 0x3FA000});
 document.body.appendChild(app.view);
+scaleToWindow(app.view);
 let tink = new Tink(PIXI, app.view);
 //Alias name
 let Loader = PIXI.loader;
@@ -85,7 +86,7 @@ let missile = {
     damageText.x = m.width - damageText.width;
     damageText.y = pointPerShootText.y + pointPerShootText.height;
 
-    m.on("click", function(e){
+    m.on("pointertap", function(e){
       if (currentMissile.damage === missile.damage) return;
       if (missile.isLocked) {
         if (missile.pointNeededToUnlock < point) {
@@ -185,7 +186,7 @@ let enemy = {
     let timer = 0;
     let isClicked = false;
     // e.on("tap", function(e){ // Mobile test
-    e.on("click", function(event){
+    e.on("pointertap", function(event){
       //handle double click enemy
       if (!isClicked) {
         app.ticker.add(tapHandler = function(delta) {
@@ -529,7 +530,7 @@ function enemyDeath(enemy, perfectCircle, greatCircle, goodCircle) {
 
   let multipler = 1;
   let i = getRandomInteger(1,100);
-  console.log(i);
+ // console.log(i);
 
   if(i<60)
     {
@@ -642,5 +643,83 @@ function contain(sprite, container) {
   if (collision.size === 0) collision = undefined;
   //Return the `collision` value
   return collision;
+}
+
+function scaleToWindow(canvas, backgroundColor) {
+  var scaleX, scaleY, scale, center;
+
+  //1. Scale the canvas to the correct size
+  //Figure out the scale amount on each axis
+  scaleX = window.innerWidth / canvas.offsetWidth;
+  scaleY = window.innerHeight / canvas.offsetHeight;
+
+  //Scale the canvas based on whichever value is less: `scaleX` or `scaleY`
+  scale = Math.min(scaleX, scaleY);
+  canvas.style.transformOrigin = "0 0";
+  canvas.style.transform = "scale(" + scale + ")";
+
+  //2. Center the canvas.
+  //Decide whether to center the canvas vertically or horizontally.
+  //Wide canvases should be centered vertically, and 
+  //square or tall canvases should be centered horizontally
+  if (canvas.offsetWidth > canvas.offsetHeight) {
+    if (canvas.offsetWidth * scale < window.innerWidth) {
+      center = "horizontally";
+    } else {
+      center = "vertically";
+    }
+  } else {
+    if (canvas.offsetHeight * scale < window.innerHeight) {
+      center = "vertically";
+    } else {
+      center = "horizontally";
+    }
+  }
+
+  //Center horizontally (for square or tall canvases)
+  var margin;
+  if (center === "horizontally") {
+    margin = (window.innerWidth - canvas.offsetWidth * scale) / 2;
+    canvas.style.marginTop = 0 + "px";
+    canvas.style.marginBottom = 0 + "px";
+    canvas.style.marginLeft = margin + "px";
+    canvas.style.marginRight = margin + "px";
+  }
+
+  //Center vertically (for wide canvases) 
+  if (center === "vertically") {
+    margin = (window.innerHeight - canvas.offsetHeight * scale) / 2;
+    canvas.style.marginTop = margin + "px";
+    canvas.style.marginBottom = margin + "px";
+    canvas.style.marginLeft = 0 + "px";
+    canvas.style.marginRight = 0 + "px";
+  }
+
+  //3. Remove any padding from the canvas  and body and set the canvas
+  //display style to "block"
+  canvas.style.paddingLeft = 0 + "px";
+  canvas.style.paddingRight = 0 + "px";
+  canvas.style.paddingTop = 0 + "px";
+  canvas.style.paddingBottom = 0 + "px";
+  canvas.style.display = "block";
+
+  //4. Set the color of the HTML body background
+  document.body.style.backgroundColor = backgroundColor;
+
+  //Fix some quirkiness in scaling for Safari
+  var ua = navigator.userAgent.toLowerCase();
+  if (ua.indexOf("safari") != -1) {
+    if (ua.indexOf("chrome") > -1) {
+      // Chrome
+    } else {
+      // Safari
+      //canvas.style.maxHeight = "100%";
+      //canvas.style.minHeight = "100%";
+    }
+  }
+
+  //5. Return the `scale` value. This is important, because you'll nee this value 
+  //for correct hit testing between the pointer and sprites
+  return scale;
 }
 
